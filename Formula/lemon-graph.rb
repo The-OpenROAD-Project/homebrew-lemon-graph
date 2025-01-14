649841c853c96e056d5db234e3c34908df307ec3
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-01-14 15:30:24 -0300

    Add revision to force install with same tag
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- Formula/lemon-graph.rb
+++ Formula/lemon-graph.rb
@@ -1,16 +1,16 @@
 class LemonGraph < Formula
-    desc "Library for Efficient Modeling and Optimization in Networks"
-    homepage "https://lemon.cs.elte.hu/trac/lemon"
-    sha256 "f829a87c357b193a97987293030cb086b8fc7ef000eac24899efd3b822f383d8"
-    head "https://github.com/The-OpenROAD-Project/lemon-graph"
-    url "https://github.com/The-OpenROAD-Project/lemon-graph/archive/refs/tags/1.3.1.tar.gz"
-    version "1.3.1"
-    license "BSL-1.0"
+  desc "Library for Efficient Modeling and Optimization in Networks"
+  homepage "https://lemon.cs.elte.hu/trac/lemon"
+  head "https://github.com/The-OpenROAD-Project/lemon-graph"
+  url "https://github.com/The-OpenROAD-Project/lemon-graph/archive/refs/tags/1.3.1.tar.gz"
+  sha256 "f829a87c357b193a97987293030cb086b8fc7ef000eac24899efd3b822f383d8"
+  revision 2
+  license "BSL-1.0"
 
-    depends_on "cmake" => :build
+  depends_on "cmake" => :build
 
-    def install
-        system "cmake", "-B", "build", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
-        system "cmake", "--build", "build", "-j", "--target", "install"
-    end
+  def install
+    system "cmake", "-B", "build", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
+    system "cmake", "--build", "build", "-j", "--target", "install"
+  end
 end
